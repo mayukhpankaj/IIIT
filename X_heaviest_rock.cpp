bbca5d47b5fe5a11c083cef6dff3387e92b11c33
mayukhpankaj <40158577+mayukhpankaj@users.noreply.github.com>
2019-10-19 22:18:57 +0530

    Create X_heaviest_rock.cpp
    test code passed.

---
+++ X_heaviest_rock.cpp
@@ -0,0 +1,34 @@
+// C++ program to sort Vector
+// in descending order
+// using sort() in STL
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n,x,xth;   //x is input.
+  vector<int> a,b;
+
+  cin >> n; //no of rocks
+
+
+
+    for (int i = 1; i <= n; i++) //vector input
+    {
+
+      cin>>x;
+      a.push_back(x);
+
+    }
+
+
+    cin>>xth; //enter xth heaviest
+
+
+	sort(a.begin(), a.end(), greater<int>()); //sort
+
+	cout<<a[xth-1];  //block weight of xth heaviest
+
+	return 0;
+}
